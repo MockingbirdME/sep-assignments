36faef09c410978add1750e75be02046724aaad7
Devon Henegar <devon.henegar@gmail.com>
2016-12-29 10:57:00 -0500

    added bucket sort

---
+++ 02-algorithms/03-sorting/bucket_sort.rb
@@ -0,0 +1,28 @@
+def bucketSort(array)
+  arry = array
+  max = findMax(arry)
+  numberOfBuckets = arry.length
+  buckets = Array.new(numberOfBuckets) { [] }
+
+  arry.length.times do |x|
+    bucketNumber = ((arry[x] * numberOfBuckets) / (max + 1))
+    buckets[bucketNumber] << arry[x]
+  end
+
+  returnArry = []
+  buckets.each do |bkt|
+    if bkt.length > 1
+      bkt = bucketSort(bkt)
+    end
+    bkt.each { |item| returnArry << item  } unless bkt.empty?
+  end
+  returnArry
+end
+
+def findMax(array)
+  max = array[0]
+  array.each do |x|
+    max = x if x > max
+  end
+  max
+end
