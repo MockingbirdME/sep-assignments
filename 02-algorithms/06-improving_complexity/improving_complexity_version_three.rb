d3212d251d2cfed7ca8efa7c2459eda197da094b
Devon Henegar <devon.henegar@gmail.com>
2017-01-05 14:21:38 -0500

    version three written, question responded to.

---
+++ 02-algorithms/06-improving_complexity/improving_complexity_version_three.rb
@@ -0,0 +1,29 @@
+def poorly_written_ruby(*arrays)
+  combined_array = []
+  arrays.each do |array|
+    array.each do |value|
+      combined_array << value
+    end
+  end
+
+  sorted_array = [combined_array.delete_at(combined_array.length-1)]
+
+  while combined_array.empty? == false
+    val = combined_array[0]
+    i = 0
+    while i < sorted_array.length
+      if val <= sorted_array[i]
+        sorted_array.insert(i, val)
+        combined_array.shift
+        break
+      elsif i == sorted_array.length - 1
+        sorted_array.insert(i, val)
+        combined_array.shift
+        break
+      end
+      i+=1
+    end
+  end
+  
+  sorted_array
+end
--- 02-algorithms/06-improving_complexity/improving_complexity_version_two.rb
+++ 02-algorithms/06-improving_complexity/improving_complexity_version_two.rb
@@ -1,28 +1,27 @@
 # This method takes n arrays as input and combine them in sorted ascending  order
- def poorly_written_ruby(*arrays)
-   combined_array = []
-   arrays.each do |array|
-     array.each do |value|
-       combined_array << value
-     end
-   end
+def poorly_written_ruby(*arrays)
+  combined_array = []
+  arrays.each do |array|
+    combined_array += array
+  end
 
-   sorted_array = [combined_array.delete_at(combined_array.length-1)]
+  sorted_array = [combined_array[0]]
 
-   for val in combined_array
-     i = 0
-     while i < sorted_array.length
-       if val <= sorted_array[i]
-         sorted_array.insert(i, val)
-         break
-       elsif i == sorted_array.length - 1
-         sorted_array.insert(i, val)
-         break
-       end
-       i+=1
-     end
-   end
+  (1..combined_array.length-1).each do |x|
+    i = 0
+    val = combined_array[x]
+    while i < sorted_array.length
+      if val <= sorted_array[i]
+        sorted_array.insert(i, val)
+        break
+      elsif val >= sorted_array[-i-1]
+        sorted_array.insert((sorted_array.length-i), val)
+        break
+      end
+      i+=1
+    end
+  end
 
-   # Return the sorted array
-   sorted_array
- end
+  # Return the sorted array
+  sorted_array
+end
